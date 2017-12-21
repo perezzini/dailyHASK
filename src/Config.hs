db4b477f7c7ac6ec5f6681eeadabcb23c9995893
Luciano Perezzini <lperezzini@gmail.com>
2017-12-21 15:17:35 -0300

    Module to retrieve values from a App configuration file

---
+++ src/Config.hs
@@ -0,0 +1,23 @@
+module Config
+    (
+    getValue
+    ) where
+
+import qualified Data.TConfig as TConfig
+import System.Directory as Dir
+
+import Data.Maybe as M
+import qualified Error as E
+
+appConfigFile :: String
+appConfigFile = "/app.cfg"
+
+getValue :: String -> IO (Maybe String)
+getValue key = do
+  currDir <- Dir.getCurrentDirectory
+  let filePath = currDir ++ appConfigFile
+  configFile <- TConfig.readConfig filePath
+  let value = TConfig.getValue key configFile
+  if M.isNothing value
+    then E.callError "Error - Config. Key not found"
+    else return (value)
