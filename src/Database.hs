872033ca25a83c12d7e265cca6c803aa57990de3
Luciano Perezzini <lperezzini@gmail.com>
2017-12-22 18:20:57 -0300

    First attempt: MongoDB abstraction

---
+++ src/Database.hs
@@ -0,0 +1,78 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Database
+    (
+    open
+    , close
+    , insert
+    , insertMany
+    , count
+    , save
+    , delete
+    ) where
+
+import qualified Database.MongoDB as MongoDB
+import qualified Config
+
+import qualified Data.Text as Text
+
+import qualified Data.Maybe as M
+
+import Control.Monad.IO.Class
+import qualified Data.Bson as Bson
+
+server :: IO String
+server = do
+  value <- Config.getValue "database.server"
+  return (M.fromJust value)
+
+port :: IO String
+port = do
+  value <- Config.getValue "database.port"
+  return (M.fromJust value)
+
+db :: IO String
+db = do
+  value <- Config.getValue "database.db"
+  return (M.fromJust value)
+
+open :: IO MongoDB.Pipe
+open = do
+  server <- server
+  pipe <- MongoDB.connect $ MongoDB.host server
+  return (pipe)
+
+close :: MongoDB.Pipe -> IO ()
+close pipe = MongoDB.close pipe
+
+run :: MongoDB.Pipe -> MongoDB.Action IO a -> IO a
+run pipe act = do
+  db <- db
+  let db' = Text.pack db
+  exec <- MongoDB.access pipe MongoDB.master db' act
+  return (exec)
+
+insert :: MongoDB.Pipe -> MongoDB.Collection -> Bson.Document -> IO Bson.Value
+insert pipe collection document = do
+  exec <- run pipe $ MongoDB.insert collection document
+  return (exec)
+
+insertMany :: MongoDB.Pipe -> MongoDB.Collection -> [Bson.Document] -> IO [Bson.Value]
+insertMany pipe collection documents = do
+  exec <- run pipe $ MongoDB.insertMany collection documents
+  return (exec)
+
+count :: MongoDB.Pipe -> MongoDB.Selector -> MongoDB.Collection -> IO Int
+count pipe fields collection = do
+  exec <- run pipe $ MongoDB.count $ MongoDB.select fields collection
+  return (exec)
+
+save :: MongoDB.Pipe -> MongoDB.Collection -> MongoDB.Selector -> IO ()
+save pipe collection fields = do
+  exec <- run pipe $ MongoDB.save collection fields
+  return (exec)
+
+delete :: MongoDB.Pipe -> MongoDB.Selector -> MongoDB.Collection -> IO ()
+delete pipe fields collection = do
+  exec <- run pipe $ MongoDB.delete $ MongoDB.select fields collection
+  return (exec)
