a472a07105d6d061a031a309dbef3e4634d7c24b
Luciano Perezzini <lperezzini@gmail.com>
2017-12-27 16:24:35 -0300

    Now, using Wreq lib to retrieve information form Google Maps API

--- src/Location.hs
+++ src/Location.hs
@@ -3,14 +3,29 @@
 module Location
     (
     GeoLoc(..)
+    , Address
+    , Latitude
+    , Longitude
     , stringToAddress
     , addressToString
     , getAddress
     , getLat
     , getLong
+    , getGeoLocFromString
     ) where
 
+import Config
+import Url
+import Utils
+import Json
+
+import Data.Aeson
+import Network.Wreq
+import Control.Lens
+
 import Data.Text as Text
+import Data.Maybe as M
+import qualified Data.Vector as V
 
 type Latitude = Double
 type Longitude = Double
@@ -21,6 +36,17 @@ data GeoLoc = GeoLoc {
   , lat :: Latitude
   , long :: Longitude
 } deriving (Show)
+
+instance FromJSON GeoLoc where
+  parseJSON = withObject "GeoLoc" $ \v -> do
+    results <- v .: "results"
+    let resultsItemObject = V.head results
+    address <- resultsItemObject .: "formatted_address"
+    geometry <- resultsItemObject .: "geometry"
+    location <- geometry .: "location"
+    lat <- location .: "lat"
+    long <- location .: "lng"
+    return (GeoLoc address lat long)
 
 getAddress :: GeoLoc -> Address
 getAddress = address
@@ -37,4 +63,24 @@ stringToAddress = Text.pack
 addressToString :: Address -> String
 addressToString = Text.unpack
 
---fromAddressToCoord :: Address -> (Latitude, Longitude)
+endpoint :: IO Url
+endpoint = do
+  value <- Config.getValue "api.googlemaps.endpoint"
+  return (M.fromJust value)
+
+key :: IO Text
+key = do
+  value <- Config.getValue "api.googlemaps.key"
+  return (Text.pack $ M.fromJust value)
+
+getGeoLocFromString :: String -> IO (Maybe GeoLoc)
+getGeoLocFromString address = do
+  endpoint <- endpoint
+  key <- key
+  let address' = Text.pack $ Utils.replaceCharInString ' ' '+' address
+  let opts = defaults & param "key" .~ [key] & param "address" .~ [address']
+  req <- getWith opts endpoint
+  let headerStatus = req ^. responseStatus . statusCode
+  if Json.requestIsOk headerStatus
+    then return (decode $ req ^. responseBody)
+    else return (Nothing)
