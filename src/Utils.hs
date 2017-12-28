e6311cf21e737db37386981fb4b84909aceac3c1
Luciano Perezzini <lperezzini@gmail.com>
2017-12-28 16:35:04 -0300

    Some utilities

---
+++ src/Utils.hs
@@ -0,0 +1,22 @@
+module Utils
+    (
+    listOfTextsToListOfStrings
+    , listOfStringsToListOfText
+    , replaceCharByCharInString
+    , connectListOfStrings
+    ) where
+
+import Data.Text as Text hiding (map)
+
+listOfStringsToListOfText :: [String] -> [Text]
+listOfStringsToListOfText list = map Text.pack list
+
+listOfTextsToListOfStrings :: [Text] -> [String]
+listOfTextsToListOfStrings list = map Text.unpack list
+
+replaceCharByCharInString :: Char -> Char -> String -> String
+replaceCharByCharInString oldChar newChar str = map (\c -> if c == oldChar then newChar else c) str
+
+connectListOfStrings :: [String] -> String -> String
+connectListOfStrings (x : []) _ = x
+connectListOfStrings (x : xs) repl = x ++ repl ++ (connectListOfStrings xs repl)
