caa1022b17f04c1618c00b7a29b7e302ef273ae8
Luciano Perezzini <lperezzini@gmail.com>
2018-01-03 20:57:02 -0300

    Attempt to complete program flow...

--- app/Main.hs
+++ app/Main.hs
@@ -4,19 +4,26 @@ module Main where
 
 import Prelude hiding (last)
 
-import Control.Concurrent
-import Control.Monad
-import Data.Time.Clock
-import System.Cron
+import Control.Concurrent (threadDelay)
+import Control.Monad (forever, when)
+import Data.Time.Clock (getCurrentTime)
+import System.Cron as Cron (daily, scheduleMatches)
 
 import Data.Maybe as M
+import Data.Text as Text hiding (words, unword, map)
+import qualified Data.Bson as Bson
+import Network.HaskellNet.SMTP.SSL as SMTP
 
 import User
 import Interest
 import Mail
+import News
 import Location
+-- import Weather
 import Html
+import Database as DB
 import Error as E
+import Config
 
 createNewGeoLoc :: String -> IO Location.GeoLoc
 createNewGeoLoc address = do
@@ -52,7 +59,42 @@ sendWelcomeMailToUser user = do
   Mail.auth conn
   Mail.send conn userEmailAddress "Welcome to dailyHASK" "plain text body" (Html.renderWelcomeMailTemplate user)
   Mail.closeConnection conn
-  return $ ()
+  return ()
+
+collection :: IO Text
+collection = do
+  value <- Config.getValue "database.usersCollection"
+  let value' = M.fromJust value
+  return $ Text.pack value'
+
+doWork :: IO ()
+doWork = let
+  workActions :: SMTPConnection -> Bson.Document -> IO ()
+  workActions conn user = do
+    let email = Bson.typed $ Bson.valueAt "email" user :: Text
+    let interests = Bson.typed $ Bson.valueAt "interests" user :: [Interest]
+    news <- News.getNews interests
+    if M.isNothing news
+      then E.callError "Error. Main: couldn't retrive news articles. Aborting..."
+      else let
+        news' = M.fromJust news
+        in Mail.send conn email "Your dailyHASK" "plain text body" (Html.renderDailyMailTemplate news')
+
+  work :: [Bson.Document] -> SMTPConnection -> IO ()
+  work users conn = do
+    mapM_ (workActions conn) users
+  in do
+    putStrLn "doWork started..."
+    collection <- collection
+    pipe <- DB.open
+    users <- DB.findAll pipe [] collection
+    DB.close pipe
+    conn <- Mail.connect
+    Mail.auth conn
+    work users conn
+    Mail.closeConnection conn
+    putStrLn "doWork finished."
+    threadDelay 10000000 -- Scaffolding...
 
 main :: IO ()
 main = do
@@ -61,13 +103,11 @@ main = do
   sendWelcomeMailToUser newUser
   putStrLn "Want to create another user? [Y/N]"
   line <- getLine
-  if line == "Y"
+  if line == "Y" || line == "y"
     then main
     else forever $ do
       now <- getCurrentTime
       when (scheduleMatches schedule now) doWork
-      putStrLn "Sleeping..."
-      --threadDelay 10000000
+      doWork
     where
-      doWork = putStrLn "Do the real work: iterate DB, retrieve information, and send e-mails, etc."
-      schedule = daily
+      schedule = Cron.daily
