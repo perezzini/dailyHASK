da8b589aae9d8d0f88b8b31469969471826050fc
Luciano Perezzini <lperezzini@gmail.com>
2018-01-24 14:34:22 -0300

    getValue: return String instead of Maybe String
    Call error in case of failure

--- src/Config.hs
+++ src/Config.hs
@@ -18,11 +18,14 @@ import qualified Error as E
 appConfigFile :: String
 appConfigFile = "/app.cfg"
 
--- |The 'getValue' function gets a value from a given key stored in /app.cfg
-getValue :: String -> IO (Maybe String)
+-- |The 'getValue' function takes a key and maybe returns its value.
+-- Exits program with error in case key could not be found
+getValue :: String -> IO String
 getValue key = do
   currDir <- Dir.getCurrentDirectory
   let filePath = currDir ++ appConfigFile
   configFile <- TConfig.readConfig filePath
   let value = TConfig.getValue key configFile
-  return $ value
+  case value of
+    Just v -> return $ v
+    otherwise -> E.callError ("Error. Key " ++ key ++ "not found in config file. Aborting...")
