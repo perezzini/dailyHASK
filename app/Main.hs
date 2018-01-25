b473a4f948f017d416f83d3f5ffd660185a9897a
Luciano Perezzini <lperezzini@gmail.com>
2018-01-25 16:25:30 -0300

    News.getNews language input not required anymore

--- app/Main.hs
+++ app/Main.hs
@@ -96,7 +96,7 @@ doWork = let
 
     let userRecord = User _id name' email location' interests :: User
 
-    news <- News.getNews interests "en"
+    news <- News.getNews interests
     currentWeather <- Weather.getCurrentWeatherFromGeoLoc $ User.getLocation userRecord
     if M.isNothing news
       then E.callError "Error. Main: couldn't retrieve news articles. Aborting..."
